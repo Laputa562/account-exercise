2d0fc605982506799965355ca705117cb9602004
Shako <auburnandivory@protonmail.com>
2025-10-30 00:49:32 +0300

    done for real this time

--- src/account.java
+++ src/account.java
@@ -1,2 +1,44 @@
 public class account {
+    private double balance = 0.0;
+    public static int incrementID = 0;
+    private int ID = 0;
+    public double setBalance(double balance) {
+        if(balance < 0.0) {
+            return this.balance;
+        }else  {
+            return balance;
+        }
+    }
+    public account(double balance) {
+        this.balance = setBalance(balance);
+        this.ID = incrementID++;
+    }
+    public double getBalance(){
+        return this.balance;
+    }
+
+    public double getID(){
+        return ID;
+    }
+
+    public void setDeposit(double deposit){
+        if(deposit < 0) {
+            System.out.println("Deposit cannot be negative");
+        }else{
+            this.balance += deposit;}
+    }
+    public void setDebit(double debit) {
+        if (debit < 0) {
+            System.out.println("Debit cannot be negative");
+        } else if (debit > this.balance) {
+            System.out.println("Debit cannot be greater than balance!!");
+        } else {
+            this.balance -= debit;
+        }
+    }
+    public void displayAccount(){
+        System.out.println("ID: " + getID());
+        System.out.println("Balance: " + getBalance());
+    }
+
 }
--- src/accountTest.java
+++ src/accountTest.java
@@ -1,2 +1,23 @@
 public class accountTest {
+    public static void main(String[] args) {
+        account ac1 = new account(5);
+        account ac2 = new account(-5);
+        ac1.displayAccount();
+        ac2.displayAccount();
+        System.out.println("----------------------------");
+        System.out.println(ac1.getBalance());
+        System.out.println(ac2.getBalance());
+        System.out.println("----------------------------");
+        ac1.setDebit(10);
+        ac2.setDeposit(-25);
+        ac1.displayAccount();
+        ac2.displayAccount();
+        System.out.println("----------------------------");
+        ac1.setDebit(2);
+        ac2.setDeposit(10);
+        ac1.displayAccount();
+        ac2.displayAccount();
+
+
+    }
 }
